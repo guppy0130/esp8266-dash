7bba881aa265f8fd4874f7e3211198e8a29aeeb7
guppy0130 <guppy0130@yahoo.com>
2023-04-21 17:46:59 -0500

    temperature chart bitmap
    * outputs bitmap at /forecast-img/ endpoint
    * arduino code updated to stream bitmap to display

--- esp8266-dash.ino
+++ esp8266-dash.ino
@@ -83,6 +83,263 @@ void render_time() {
     display.print(buffer);
 }
 
+uint16_t read16(std::unique_ptr<WiFiClient> &client) {
+    // BMP data is stored little-endian, same as Arduino.
+    uint16_t result;
+    ((uint8_t *)&result)[0] = client->read();  // LSB
+    ((uint8_t *)&result)[1] = client->read();  // MSB
+    // Serial.printf("read16: %x\n", result);
+    return result;
+}
+
+int32_t read32(std::unique_ptr<WiFiClient> &client) {
+    // BMP data is stored little-endian, same as Arduino.
+    int32_t result;
+    ((uint8_t *)&result)[0] = client->read();  // LSB
+    ((uint8_t *)&result)[1] = client->read();
+    ((uint8_t *)&result)[2] = client->read();
+    ((uint8_t *)&result)[3] = client->read();  // MSB
+    // Serial.printf("read32: %x\n", result);
+    return result;
+}
+
+uint32_t skip(std::unique_ptr<WiFiClient> &client, int32_t bytes) {
+    int32_t remain = bytes;
+    uint32_t start = millis();
+    while ((client->connected() || client->available()) && (remain > 0)) {
+        if (client->available()) {
+            int16_t v = client->read();
+            (void)v;
+            remain--;
+        } else
+            delay(1);
+        if (millis() - start > 2000) break;  // don't hang forever
+    }
+    return bytes - remain;
+}
+
+uint32_t read8n(std::unique_ptr<WiFiClient> &client, uint8_t *buffer, int32_t bytes) {
+    int32_t remain = bytes;
+    uint32_t start = millis();
+    while ((client->connected() || client->available()) && (remain > 0)) {
+        if (client->available()) {
+            int16_t v = client->read();
+            *buffer++ = uint8_t(v);
+            remain--;
+        } else
+            delay(1);
+        if (millis() - start > 2000) break;  // don't hang forever
+    }
+    return bytes - remain;
+}
+
+static const uint16_t input_buffer_pixels = 800;       // may affect performance
+static const uint16_t max_row_width = 400;             // for up to 4.2" display 400x300
+static const uint16_t max_palette_pixels = 256;        // for depth <= 8
+uint8_t input_buffer[3 * input_buffer_pixels];         // up to depth 24
+uint8_t output_row_mono_buffer[max_row_width / 8];     // buffer for at least one row of b/w bits
+uint8_t output_row_color_buffer[max_row_width / 8];    // buffer for at least one row of color bits
+uint8_t mono_palette_buffer[max_palette_pixels / 8];   // palette buffer for depth <= 8 b/w
+uint8_t color_palette_buffer[max_palette_pixels / 8];  // palette buffer for depth <= 8 c/w
+
+void draw_bitmap_http(const char *host, uint16_t port, char *path, int16_t x, int16_t y, bool with_color) {
+    std::unique_ptr<WiFiClient> client(new WiFiClient);
+    HTTPClient http;
+    if (!client->connect(host, port)) {
+        Serial.printf("Unable to connect to %s:%d", host, port);
+        return;
+    }
+
+    bool connection_ok = false;
+    bool valid = false;  // valid format to be handled
+    bool flip = true;    // bitmap is stored bottom-to-top
+
+    client->print(String("GET ") + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+    // read headers
+    while (client->connected()) {
+        String line = client->readStringUntil('\n');
+        Serial.println(line);
+        if (!connection_ok) {
+            connection_ok = line.startsWith("HTTP/1.1 200");
+        }
+        if (line == "\r") {
+            Serial.println("headers received");
+            break;
+        }
+    }
+    if (!connection_ok) return;
+    // wait for data to be available, if the server is a little slow
+    while (!client->available()) {
+        delay(1);
+    }
+    if (read16(client) == 0x4D42) {  // BMP signature
+        int32_t fileSize = read32(client);
+        int32_t creatorBytes = read32(client);
+        int32_t imageOffset = read32(client);  // Start of image data
+        int32_t headerSize = read32(client);
+        int32_t width = read32(client);
+        int32_t height = read32(client);
+        uint16_t planes = read16(client);
+        uint16_t depth = read16(client);  // bits per pixel
+        int32_t format = read32(client);
+        int32_t bytes_read = 7 * 4 + 3 * 2;  // read so far
+        (void)creatorBytes;
+        if ((planes == 1) && ((format == 0) || (format == 3))) {  // uncompressed is handled, 565 also
+            Serial.print("File size: ");
+            Serial.println(fileSize);
+            Serial.print("Image Offset: ");
+            Serial.println(imageOffset);
+            Serial.print("Header size: ");
+            Serial.println(headerSize);
+            Serial.print("Bit Depth: ");
+            Serial.println(depth);
+            Serial.print("Image size: ");
+            Serial.print(width);
+            Serial.print('x');
+            Serial.println(height);
+            // BMP rows are padded (if needed) to 4-byte boundary
+            uint32_t rowSize = (width * depth / 8 + 3) & ~3;
+            if (depth < 8) rowSize = ((width * depth + 8 - depth) / 8 + 3) & ~3;
+            if (height < 0) {
+                height = -height;
+                flip = false;
+            }
+            uint16_t w = width;
+            uint16_t h = height;
+            if ((x + w - 1) >= display.width()) w = display.width() - x;
+            if ((y + h - 1) >= display.height()) h = display.height() - y;
+            valid = true;
+            Serial.print("valid: ");
+            Serial.println(valid);
+            uint8_t bitmask = 0xFF;
+            uint8_t bitshift = 8 - depth;
+            uint16_t red, green, blue;
+            bool whitish = false;
+            bool colored = false;
+            if (depth == 1) with_color = false;
+            if (depth <= 8) {
+                if (depth < 8) bitmask >>= depth;
+                // bytes_read += skip(client, 54 - bytes_read); //palette is
+                // always @ 54
+                bytes_read += skip(client, imageOffset - (4 << depth) - bytes_read);  // 54 for regular, diff
+                                                                                      // for colorsimportant
+                for (uint16_t pn = 0; pn < (1 << depth); pn++) {
+                    blue = client->read();
+                    green = client->read();
+                    red = client->read();
+                    client->read();
+                    bytes_read += 4;
+                    whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80);  // whitish
+                    colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0));                                                   // reddish or yellowish?
+                    if (0 == pn % 8) mono_palette_buffer[pn / 8] = 0;
+                    mono_palette_buffer[pn / 8] |= whitish << pn % 8;
+                    if (0 == pn % 8) color_palette_buffer[pn / 8] = 0;
+                    color_palette_buffer[pn / 8] |= colored << pn % 8;
+                }
+            }
+            uint32_t rowPosition = flip ? imageOffset + (height - h) * rowSize : imageOffset;
+            // Serial.print("skip "); Serial.println(rowPosition - bytes_read);
+            bytes_read += skip(client, rowPosition - bytes_read);
+            for (uint16_t row = 0; row < h; row++, rowPosition += rowSize)  // for each line
+            {
+                if (!connection_ok || !(client->connected() || client->available())) break;
+                delay(1);  // yield() to avoid WDT
+                uint32_t in_remain = rowSize;
+                uint32_t in_idx = 0;
+                uint32_t in_bytes = 0;
+                uint8_t in_byte = 0;  // for depth <= 8
+                uint8_t in_bits = 0;  // for depth <= 8
+                uint16_t color = GxEPD_WHITE;
+                for (uint16_t col = 0; col < w; col++)  // for each pixel
+                {
+                    yield();
+                    if (!connection_ok || !(client->connected() || client->available())) break;
+                    // Time to read more pixel data?
+                    if (in_idx >= in_bytes)  // ok, exact match for 24bit
+                                             // also (size IS multiple of 3)
+                    {
+                        uint32_t get = in_remain > sizeof(input_buffer) ? sizeof(input_buffer) : in_remain;
+                        uint32_t got = read8n(client, input_buffer, get);
+                        while ((got < get) && connection_ok) {
+                            // Serial.print("got ");
+                            // Serial.print(got);
+                            // Serial.print(" < ");
+                            // Serial.print(get);
+                            // Serial.print(" @ ");
+                            // Serial.println(bytes_read);
+                            uint32_t gotmore = read8n(client, input_buffer + got, get - got);
+                            got += gotmore;
+                            connection_ok = gotmore > 0;
+                        }
+                        in_bytes = got;
+                        in_remain -= got;
+                        bytes_read += got;
+                    }
+                    if (!connection_ok) {
+                        Serial.print("Error: got no more after ");
+                        Serial.print(bytes_read);
+                        Serial.println(" bytes read!");
+                        break;
+                    }
+                    switch (depth) {
+                        case 24:
+                            blue = input_buffer[in_idx++];
+                            green = input_buffer[in_idx++];
+                            red = input_buffer[in_idx++];
+                            whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80);  // whitish
+                            colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0));  // reddish or yellowish?
+                            break;
+                        case 16: {
+                            uint8_t lsb = input_buffer[in_idx++];
+                            uint8_t msb = input_buffer[in_idx++];
+                            if (format == 0)  // 555
+                            {
+                                blue = (lsb & 0x1F) << 3;
+                                green = ((msb & 0x03) << 6) | ((lsb & 0xE0) >> 2);
+                                red = (msb & 0x7C) << 1;
+                            } else  // 565
+                            {
+                                blue = (lsb & 0x1F) << 3;
+                                green = ((msb & 0x07) << 5) | ((lsb & 0xE0) >> 3);
+                                red = (msb & 0xF8);
+                            }
+                            whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80);  // whitish
+                            colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0));  // reddish or yellowish?
+                        } break;
+                        case 1:
+                        case 4:
+                        case 8: {
+                            if (0 == in_bits) {
+                                in_byte = input_buffer[in_idx++];
+                                in_bits = 8;
+                            }
+                            uint16_t pn = (in_byte >> bitshift) & bitmask;
+                            whitish = mono_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                            colored = color_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                            in_byte <<= depth;
+                            in_bits -= depth;
+                        } break;
+                    }
+                    if (whitish) {
+                        color = GxEPD_WHITE;
+                    } else if (colored && with_color) {
+                        color = GxEPD_RED;
+                    } else {
+                        color = GxEPD_BLACK;
+                    }
+                    uint16_t yrow = y + (flip ? h - row - 1 : row);
+                    display.drawPixel(x + col, yrow, color);
+                }  // end pixel
+            }      // end line
+        }
+        Serial.print("bytes read ");
+        Serial.println(bytes_read);
+    }
+    if (!valid) {
+        Serial.println("bitmap format not handled.");
+    }
+}
+
 /**
  * Fetch and render the weather to display + serial.
  *
@@ -92,21 +349,9 @@ void render_time() {
 void render_weather() {
     // TODO: switch to using HTTPS if you don't trust your server. Note that
     // you will pay CPU cycles.
-    std::unique_ptr<WiFiClient> client(new WiFiClient);
-    HTTPClient http;
-    char url[128];
-    snprintf(url, 128, "%s/forecast/%.3f,%.3f", SERVER_URL, lat, lon);
-    if (http.begin(*client, url)) {
-        int httpCode = http.GET();
-        Serial.printf("Received %d from %s\n", httpCode, url);
-        if (httpCode != HTTP_CODE_OK) {
-            return;
-        }
-        String s = http.getString();
-        display.setCursor(0, DISPLAY_MARGIN + 9);
-        display.println(s);
-        Serial.println(s);
-    }
+    char path[128];
+    snprintf(path, 128, "/forecast-img/%.3f,%.3f", lat, lon);
+    draw_bitmap_http(SERVER_HOST, SERVER_PORT, path, 0, 0, false);
 }
 
 void loop() {
--- src/api.py
+++ src/api.py
@@ -1,14 +1,22 @@
-from datetime import datetime
-from datetime import timezone
+from collections import defaultdict
+from datetime import datetime, timezone, tzinfo
 from importlib.metadata import distribution
+from io import BytesIO
 from typing import List
 
-from fastapi import FastAPI
+import seaborn
+from fastapi import FastAPI, Response
 from fastapi.responses import PlainTextResponse
+from matplotlib.axes import Axes
+from matplotlib.dates import DateFormatter
+from matplotlib import pyplot
 from noaa_sdk import NOAA
 from pydantic import parse_obj_as
+from PIL import Image
 
 from weather_models import NOAAForecastModel
+
+pyplot.switch_backend("Agg")
 
 app_distribution = distribution("esp8266-dash-server").metadata
 
@@ -25,6 +33,22 @@ def get_root():
     return {"status": "ok"}
 
 
+def _get_hourly_forecast(lat: float, lon: float):
+    response = noaa.points_forecast(lat=lat, long=lon, type="forecastHourly")
+    forecasts = parse_obj_as(
+        List[NOAAForecastModel],
+        response["properties"]["periods"],
+    )
+    # sometimes, NOAA might reply with a forecast in the past.
+    return sorted(
+        filter(
+            lambda forecast: forecast.start_time >= datetime.now(timezone.utc),
+            forecasts,
+        ),
+        key=lambda forecast: forecast.start_time,
+    )
+
+
 @app.get("/forecast/{lat},{lon}", response_class=PlainTextResponse)
 def get_hourly_forecast(lat: float, lon: float) -> str:
     """
@@ -33,18 +57,11 @@ def get_hourly_forecast(lat: float, lon: float) -> str:
 
     You should customize the number of hourly forecasts returned, etc.
     """
-    response = noaa.points_forecast(lat=lat, long=lon, type="forecastHourly")
-    forecasts = parse_obj_as(
-        List[NOAAForecastModel],
-        response["properties"]["periods"],
-    )
 
-    # sometimes, NOAA might reply with a forecast in the past. also, we only
-    # want the first 4 forecasts so that it fits on the display.
+    # we only want the first 4 forecasts so that it fits on the display.
     serialized_forecasts = [
         str(forecast).split("\n")
-        for forecast in forecasts
-        if forecast.start_time >= datetime.now(timezone.utc)
+        for forecast in _get_hourly_forecast(lat=lat, lon=lon)
     ][:4]
 
     # determine longest str
@@ -60,3 +77,91 @@ def get_hourly_forecast(lat: float, lon: float) -> str:
             # center every value relative to its cell
             serialized_forecasts[i][j] = text.center(longest_str_len)
     return "\n".join("|".join(f) for f in zip(*serialized_forecasts))
+
+
+@app.get(
+    "/forecast-img/{lat},{lon}",
+    responses={200: {"content": {"image/bmp": {}}}},
+    response_class=Response,
+)
+def get_hourly_forecast_image(lat: float, lon: float):
+    """
+    Returns a 400x200x1 bitmap for 7 days.
+    """
+    forecast = _get_hourly_forecast(lat=lat, lon=lon)
+
+    # restructure data
+    data = defaultdict(list)
+    # next 24 hours only?
+    max_hours = 24
+    for idx, f in enumerate(forecast):
+        if idx >= max_hours:
+            break
+        for k, v in f.dict().items():
+            data[k].append(v)
+    # this should always be present from NOAA reply
+    _tz: tzinfo = data["start_time"][0].tzinfo
+
+    # generate plot + graph results
+    seaborn.set_palette(["black"], 1)
+    axes: Axes = seaborn.lineplot(
+        data=data,
+        x="start_time",
+        y="temperature",
+    )
+
+    # show min/max
+    min_temp_idx = data["temperature"].index(min(data["temperature"]))
+    axes.vlines(
+        x=data["start_time"][min_temp_idx],
+        ymin=min(data["temperature"]) - 1,
+        ymax=min(data["temperature"]),
+        linestyles="dashed",
+    )
+    axes.text(
+        data["start_time"][min_temp_idx],
+        0.03,
+        f" {min(data['temperature'])}",
+        # ha="center",
+        transform=axes.get_xaxis_transform(),
+    )
+    max_temp_idx = data["temperature"].index(max(data["temperature"]))
+    axes.vlines(
+        x=data["start_time"][max_temp_idx],
+        ymin=min(data["temperature"]) - 1,
+        ymax=max(data["temperature"]),
+        linestyles="dashed",
+    )
+    axes.text(
+        data["start_time"][max_temp_idx],
+        0.03,
+        f" {max(data['temperature'])}",
+        # ha="center",
+        transform=axes.get_xaxis_transform(),
+    )
+
+    # hide labels, set title
+    axes.set(xlabel=None, ylabel=None, title="Temperature")
+    axes.xaxis.set_major_formatter(DateFormatter("%H", tz=_tz))
+
+    # matplotlib won't let us save to bmp directly, so save to intermediate
+    # format, then read in intermediate format and use pillow to convert to
+    # 1-depth bmp
+    with BytesIO() as png_buf, BytesIO() as bmp_buf:
+        fig = axes.get_figure()
+        # set the image dimensions here
+        fig.set_size_inches(4, 2)
+        fig.set_dpi(100)
+        fig.savefig(
+            png_buf,
+            transparent=True,
+            format="png",
+            pil_kwargs={"optimize": True},
+        )
+        fig.clear()
+        with Image.open(png_buf) as im:
+            im.convert("1", colors=2).save(
+                bmp_buf, format="bmp", optimize=True
+            )
+
+        return Response(content=bmp_buf.getvalue(), media_type="image/bmp")
